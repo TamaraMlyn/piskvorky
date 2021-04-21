9d3b577b9750ee258b5a7fcc89d4c757b63ec548
Tamara Mlýnková <tamara.mlynkova@gmail.com>
2021-04-21 10:39:04 +0200

    Úkol č. 4

--- hra.js
+++ hra.js
@@ -1,10 +1,26 @@
-'use strict'
+'use strict';
 console.log('funguju');
 
-const jeNaTahu = 'circle';
 
-const policko = document.querySelector('button');
+let activePlayer = 'circle';
 
-if (jeNaTahu === 'circle') {
-  policko.addEventListener('click', policko.classList.add('board__field--circle'));
-}
+const btnElm = document.querySelectorAll('button');
+const playerIconElm = document.querySelector('.icon-active-player');
+
+const addIcon = (event) => {
+    if (activePlayer === 'circle') {
+      event.target.classList.add('board__field--circle');
+      playerIconElm.src = 'images/circle.svg'
+      event.target.disabled = true;
+      activePlayer = 'cross';
+    } else {
+      event.target.classList.add('board__field--cross');
+      playerIconElm.src = 'images/cross.svg'
+      event.target.disabled = true;
+      activePlayer = 'circle';
+    }
+  };
+
+for (let i = 0; i < btnElm.length; i++) {
+  btnElm[i].addEventListener('click', addIcon);
+}; 
