a9c9ce09d7167d754a5e3cb315072f027c166c68
Tamara Mlýnková <tamara.mlynkova@gmail.com>
2021-04-21 11:11:12 +0200

    Úprava alt atributu

--- hra.js
+++ hra.js
@@ -10,15 +10,16 @@ const playerIconElm = document.querySelector('.icon-active-player');
 const addIcon = (event) => {
     if (activePlayer === 'circle') {
       event.target.classList.add('board__field--circle');
-      playerIconElm.src = 'images/circle.svg'
+      playerIconElm.src = 'images/cross.svg'
       event.target.disabled = true;
       activePlayer = 'cross';
     } else {
       event.target.classList.add('board__field--cross');
-      playerIconElm.src = 'images/cross.svg'
+      playerIconElm.src = 'images/circle.svg'
       event.target.disabled = true;
       activePlayer = 'circle';
     }
+    playerIconElm.alt = `Právě je na tahu ${activePlayer}`
   };
 
 for (let i = 0; i < btnElm.length; i++) {
